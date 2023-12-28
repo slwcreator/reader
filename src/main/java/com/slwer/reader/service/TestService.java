7b4c8a157b074dc7327fdae36d3e11e7fe072f48
slwer <1047579002@qq.com>
2023-12-28 16:05:06 +0800

    feat: 集成声明式事务

---
+++ src/main/java/com/slwer/reader/service/TestService.java
@@ -0,0 +1,23 @@
+package com.slwer.reader.service;
+
+import com.slwer.reader.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Service
+public class TestService {
+    @Resource
+    private TestMapper testMapper;
+
+    @Transactional(rollbackFor = Exception.class)
+    public void batchImport() {
+        for (int i = 0; i < 5; i++) {
+            /*if (i == 2) {
+                throw new RuntimeException("未处理异常");
+            }*/
+            testMapper.insertSample();
+        }
+    }
+}
