70e168fc933635757143c9a6e0214f0ff7afcb85
slwer <1047579002@qq.com>
2024-02-01 10:32:28 +0800

    feat: 实现会员注册功能

--- src/main/java/com/slwer/reader/controller/MemberController.java
+++ src/main/java/com/slwer/reader/controller/MemberController.java
@@ -1,15 +1,21 @@
 package com.slwer.reader.controller;
 
+import com.slwer.reader.entity.Member;
+import com.slwer.reader.service.MemberService;
 import com.slwer.reader.utils.ResponseUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/member")
 public class MemberController {
+    @Resource
+    MemberService memberService;
+
     @PostMapping("/registe")
     public ResponseUtils registe(String username, String password, String nickname, String vc, HttpServletRequest request) {
         String verifyCode = (String) request.getSession().getAttribute("kaptchaVerifyCode");
@@ -17,7 +23,13 @@ public class MemberController {
         if (verifyCode == null || !verifyCode.equalsIgnoreCase(vc)) {
             resp = new ResponseUtils("VerifyCodeError", "验证码错误");
         } else {
-            resp = new ResponseUtils();
+            //验证码校验通过后进行用户注册
+            try {
+                Member member = memberService.createMember(username, password, nickname);
+                resp = new ResponseUtils();
+            } catch (Exception e) {
+                resp = new ResponseUtils(e.getClass().getSimpleName(), e.getMessage());
+            }
         }
         return resp;
     }
