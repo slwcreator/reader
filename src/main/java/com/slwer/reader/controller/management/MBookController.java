c504cc7d8bda03f19758c81c5a0111b5f5a63e4f
slwer <1047579002@qq.com>
2024-02-23 17:22:51 +0800

    feat: 开发 wangEditor 文件上传功能

--- src/main/java/com/slwer/reader/controller/management/MBookController.java
+++ src/main/java/com/slwer/reader/controller/management/MBookController.java
@@ -3,12 +3,19 @@ package com.slwer.reader.controller.management;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.slwer.reader.service.BookService;
 import com.slwer.reader.utils.ResponseUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/management/book")
@@ -36,5 +43,21 @@ public class MBookController {
             resp = new ResponseUtils(e.getClass().getSimpleName(), e.getMessage());
         }
         return resp;
+    }
+
+    @PostMapping("/upload")
+    public Map<String, Object> upload(@RequestParam("img") MultipartFile file, HttpServletRequest request) throws IOException {
+        //得到文件上传目录
+        String uploadPath = request.getServletContext().getResource("/").getPath() + "/upload/";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        String fileName = sdf.format(new Date());
+
+        String originalFilename = file.getOriginalFilename();
+        String suffix = Objects.requireNonNull(originalFilename).substring(originalFilename.lastIndexOf("."));
+        file.transferTo(new File(uploadPath + fileName + suffix));
+        Map<String, Object> result = new LinkedHashMap<>();
+        result.put("errno", 0);
+        result.put("data", new String[]{"/upload/" + fileName + suffix});
+        return result;
     }
 }
